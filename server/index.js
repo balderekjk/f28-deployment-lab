672cb28a67d5b60c66bf9342514abd972f0c7b1e
Derek Bal <bal.derek.j@gmail.com>
2022-09-21 11:09:56 -1000

    boilerplate finished

---
+++ server/index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+
+const path = require('path');
+
+require('dotenv').config();
+
+const port = process.env.PORT || 4000;
+
+const app = express();
+
+app.use(express.json());
+
+app.use('/', express.static(path.join(__dirname, '../client/index.html')));
+
+app.use(express.static(path.join(__dirname, '../client')));
+
+// app.get('/', (req, res) => {
+//   res.sendFile(path.join(__dirname, './index.html'));
+// });
+
+app.listen(port, () => console.log(`Docked at port ${port}`));
