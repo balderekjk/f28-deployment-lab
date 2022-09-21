06643322b9c2c5fdf24ae97e94e0427693639c9e
Derek Bal <bal.derek.j@gmail.com>
2022-09-21 12:32:16 -1000

    added some features

--- client/index.js
+++ client/index.js
@@ -1,1 +1,18 @@
-console.log('It works!');
+const input = document.querySelector('input');
+const submit = document.querySelector('button');
+const emailConfirm = document.querySelector('#email-confirm');
+let inputVal = '';
+
+const updateInputVal = (e) => {
+  inputVal = e.target.value;
+};
+
+const handleSubmit = (e) => {
+  e.preventDefault;
+  let confirmMsg = document.createElement('p');
+  confirmMsg.textContent = `Email: ${inputVal}, was successfully added to email list`;
+  emailConfirm.appendChild(confirmMsg);
+};
+
+input.addEventListener('keyup', updateInputVal);
+submit.addEventListener('click', handleSubmit);
