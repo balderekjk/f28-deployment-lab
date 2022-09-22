7295bcf0d1dd9fbda757d13ba97de05a3a17241f
Derek Bal <bal.derek.j@gmail.com>
2022-09-22 12:44:00 -1000

    initial rollbar setup

--- server/index.js
+++ server/index.js
@@ -9,6 +9,18 @@ const port = process.env.PORT || 4000;
 const app = express();
 
 app.use(express.json());
+app.use(cors());
+
+// include and initialize the rollbar library with your access token
+var Rollbar = require('rollbar');
+var rollbar = new Rollbar({
+  accessToken: 'e4d29d63fd914b74930b22290c0a5a8c',
+  captureUncaught: true,
+  captureUnhandledRejections: true,
+});
+
+// record a generic message and send it to Rollbar
+rollbar.log('Hello world!');
 
 app.use('/', express.static(path.join(__dirname, '../client/index.html')));
 
